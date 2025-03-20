46f1af9ac2531526c0e3f909af325208262ec0cf
Joseph Potashnik <joseph.potashnik@gmail.com>
2025-03-20 16:55:18 +0000

    fetching parse from the backend!

--- src/App.tsx
+++ src/App.tsx
@@ -2,33 +2,107 @@ import './App.css'
 import Vertex from './components/Vertex.tsx'
 import { Node } from './components/Rule.ts'
 import { parseToJSON} from './parseBracketedStrings.ts'
+import { JSX, useEffect, useState } from "react";
 
 function parseNode(data: any): Node {
   const node = new Node(data.LHS, data.RHS.map(parseNode)); // Recursively create nodes
   return node;
 }
 
+const grammarRules = [
+  "START -> T1",
+"T1 -> NP VP",
+"VP -> V0",
+"VP -> V1 NP",
+"VP -> V2 PP", 
+"VP -> V3 T1",
+"PP -> P NP",
+"NP -> D N",
+"NP -> PN",
+"NP -> NP PP",
+"VP -> VP PP"
+];
+
+const PartOfSpeechRules = [
+  "PN -> John",
+  "V1 -> loved",
+  "D -> the",
+  "N -> girl",
+  "P -> with",
+  "N -> bells"
+]
+
+const sentence = "the girl loved John with the bells";
+
+interface EarleyParserParameters
+{
+  GrammarRules : string[],
+  PartOfSpeechRules : string[],
+  Sentence : string
+}
+
+async function parseSentence() : Promise<string[]> 
+{
+
+  const bodyData : EarleyParserParameters = 
+  {
+    GrammarRules : grammarRules,
+    PartOfSpeechRules : PartOfSpeechRules,
+    Sentence : sentence
+  }
+
+  const url = "http://localhost:5158/";
+  const response = await fetch(url+"ParseSentence/", {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(bodyData) 
+  });
+
+  const data = (await response.json()) as string[];
+  console.log(data);
+  return data;
+
+}
 function App() {
   
-  //const str1 = "(START (T1 (NP (PN John)) (VP (VP (V1 loved) (NP (D the) (N girl))) (PP (P with) (NP (D the) (N bells))))))";
-  const str1 = "(START (T1 (NP (PN John)) (VP (V1 loved) (NP (NP (D the) (N girl)) (PP (P with) (NP (D the) (N bells)))))))";
-  const treee = parseToJSON(str1);
+  const [data, setData] = useState<string[]|null>();
 
-  const node : Node = parseNode(treee);
-  node.assignDepths(0);
-  const width = node.assignWidths(0);
-  const depth : number = node.getMaxDepth();
-  const svgWidth : number = 800;
-  const svgHeight : number = 600;
-  const spacingX : number = svgWidth / (width -1);
-  const spacingY : number = svgHeight / (depth -1);
+  useEffect(() => {
+    async function fetchData() {
+
+    const results : string[] = await parseSentence();
+    setData(results);
+    }
+    fetchData();
+  }, []); // runs only once.
+
+  let svg : JSX.Element;
+
+  if (data != null)
+  {
+    const treee = parseToJSON(data[0]);
+    const node : Node = parseNode(treee);
+    node.assignDepths(0);
+    const width = node.assignWidths(0);
+    const depth : number = node.getMaxDepth();
+    const svgWidth : number = 800;
+    const svgHeight : number = 600;
+    const spacingX : number = svgWidth / (width -1);
+    const spacingY : number = svgHeight / (depth -1);
+
+    svg = <svg width={svgWidth} height={svgHeight} viewBox={`-50 -50 ${svgWidth+100} ${svgHeight+100}`} xmlns="http://www.w3.org/2000/svg"> 
+
+    <Vertex node = {node} spacing = {[spacingX, spacingY]}> </Vertex>
+   </svg>;
+  }
+  else
+  {
+    svg = <p> Loading... </p>;
+  }
 
   return (
     <>
-   <svg width={svgWidth} height={svgHeight} viewBox={`-50 -50 ${svgWidth+100} ${svgHeight+100}`} xmlns="http://www.w3.org/2000/svg"> 
-
-      <Vertex node = {node} spacing = {[spacingX, spacingY]}> </Vertex>
-     </svg>
+    {svg}
     </> 
   )
 }
